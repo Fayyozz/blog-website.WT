06913d2c71b6352ab80741a353fe7ab746012e66
Fayyozbek <fayyozbekk@gmail.com>
2021-04-05 16:38:54 +0500

    finishing the backend of the project

--- app.js
+++ app.js
@@ -1,29 +1,85 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
+app.use(express.urlencoded({extended: false}))
 app.set("view engine", "pug");
+
 app.use("/static", express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("home");
 });
 
 app.get("/create", (req, res) => {
-    res.render("create")
+    res.render("create")   
 })
+
+app.post("/create", (req, postRes) => {
+    const title = req.body.title;
+    const blogBody = req.body.blogBody;
+
+    if (title.trim() === "" && blogBody.trim() === "") {
+        postRes.render("create", {error: true})
+    } else {
+        fs.readFile("./data/blogs.json", (err, data) => {
+            if (err) throw err;
+
+            const blogsData = JSON.parse(data);
+
+            blogsData.push({
+                id: id(),
+                title: title,
+                blogBody: blogBody
+            })
+
+            fs.writeFile("./data/blogs.json", JSON.stringify(blogsData), (err) => {
+                if (err) throw err
+
+                postRes.render("create", {success: true})
+            })
+        })
+    }
+})
+
 
 app.get("/blogs", (req, res) => {
-    res.render("blogs", {blogs: blogs})
+    fs.readFile("./data/blogs.json", (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        res.render("blogs", {blogs: blogs})
+    })
+
+   
 })
 
-app.get("/blogs/detail", (req, res) => {
-    res.render("detail")
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile("./data/blogs.json", (err, data) => {
+        if (err) throw err
+
+        const blogs = JSON.parse(data)
+
+        const blog = blogs.filter(blog => blog.id == id)[0]
+
+        res.render("detail",  {blog: blog} )
+
+    })
+
 })
 
-const blogs = ["sfgsdfg ", "dfshethaet"]
 
 app.listen(3000, err => {
     if (err) console.log(err);
 
-    console.log("serser is running on port 3000...");
+    console.log("server is running on port 3000...");
 })
+
+function id () {
+    return "_" + Math.random().toString(36).substr(2, 9);
+}
 
